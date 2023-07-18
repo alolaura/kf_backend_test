3ea70a2a515b01834d47225c6b3939699d0fdb2c
Laura Mihai <lauramihaelamihai@gmail.com>
2023-07-18 09:02:41 +0100

    update jest config and add coverage to gitignore

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,9 @@ import type { Config } from 'jest';
 const config: Config = {
   verbose: true,
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  collectCoverage: true,
+  testMatch: ['**/?(*.)+(spec|test).ts']
 };
 
 export default config;
